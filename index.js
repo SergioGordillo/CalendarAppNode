729195799a20373c864dd886be6d2e6d58da85e6
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-03 12:19:09 +0100

    feat: basic configuration of express done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    console.log("get working");
+    res.json({
+        ok: true
+    })
+})
+
+
+app.listen(4000, () => {
+    console.log(`Server running on port ${4000}`);
+})
