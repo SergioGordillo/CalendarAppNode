72bfffc91c968b8c137502e429f2c9ebe9d842b3
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-05 23:47:02 +0100

    feat: PoC of auth into routes folder implemented

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.static("public"));
-
-
-// app.get("/", (req, res) => {
-//     console.log("get working");
-//     res.json({
-//         ok: true
-//     })
-// })
-
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
