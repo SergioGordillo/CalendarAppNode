36b0b9c417753fef3cae868beb982b6168922502
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-07 23:27:35 +0100

    feat: some improvements in auth post requests

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 const { response } = require('express');
 
 const register = (req, res = response) => {
+
+    const { name, email, password } = req.body; 
+
+    if ( name.length <5) {
+        return res.status(400).json({
+            ok: false,
+            msg: "The name must have as minimum 5 letters"
+        });
+    }
+
     res.json({
         ok: true,
-        msg: "Register"
+        msg: "Register",
+        name,
+        email,
+        password
     })
 }
 
 const login = (req, res = response) => {
+
+    const { email, password } = req.body; 
+
     res.json({
         ok: true,
-        msg: "Login"
+        msg: "Login",
+        email,
+        password
     })
 };
 
