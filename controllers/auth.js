750a043818ac000c788b0fd6e55af37085b12e74
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-08 11:47:12 +0100

    feat: validations added to auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require("express-validator");
 
 const register = (req, res = response) => {
 
     const { name, email, password } = req.body; 
 
-    if ( name.length <5) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
         return res.status(400).json({
             ok: false,
-            msg: "The name must have as minimum 5 letters"
-        });
+            errors: errors.mapped()
+        })
     }
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: "Register",
         name,
@@ -24,7 +27,16 @@ const login = (req, res = response) => {
 
     const { email, password } = req.body; 
 
-    res.json({
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
+    res.status(201).json({
         ok: true,
         msg: "Login",
         email,
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,25 @@
 const { Router } = require('express');
 const router = Router(); 
+const { check } = require("express-validator");
 
 const { register, login, renew } = require("../controllers/auth");
 
-router.post("/new", register);
+router.post(
+    "/new",
+    [
+        check("name", "You need a name to register").not().isEmpty(),
+        check("email", "You need an email to register").isEmail(),
+        check("password", "You need a password with at least 6 letters to register").isLength({min:6})
+    ], 
+    register);
 
-router.post("/", login)
+router.post(
+    "/",
+    [
+        check("email", "You need an email to login").isEmail(),
+        check("password", "You need a password to login").isLength({min:6})
+    ], 
+    login)
 
 router.get("/renew", renew)
 
