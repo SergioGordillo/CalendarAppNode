fc4af079ab2254f308cf49e6a2ac302f2bded233
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-09 11:48:39 +0100

    feat: implementation of custom middleware for the validations of fields done

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,15 @@ const router = Router();
 const { check } = require("express-validator");
 
 const { register, login, renew } = require("../controllers/auth");
+const { fieldsValidator } = require('../middlewares/fields-validator');
 
 router.post(
     "/new",
     [
         check("name", "You need a name to register").not().isEmpty(),
         check("email", "You need an email to register").isEmail(),
-        check("password", "You need a password with at least 6 letters to register").isLength({min:6})
+        check("password", "You need a password with at least 6 letters to register").isLength({min:6}),
+        fieldsValidator
     ], 
     register);
 
@@ -17,7 +19,8 @@ router.post(
     "/",
     [
         check("email", "You need an email to login").isEmail(),
-        check("password", "You need a password to login").isLength({min:6})
+        check("password", "You need a password to login").isLength({min:6}),
+        fieldsValidator
     ], 
     login)
 
