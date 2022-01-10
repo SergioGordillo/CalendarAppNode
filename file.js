87ef7369cfe3bece1ec8d6bec78dbf83099610e7
Sergio Gordillo <sergiogordillodev@gmail.com>
2022-01-10 20:52:00 +0100

    feat: installation of Mongoose and connection between Node and MongoDB done

---
+++ file.js
@@ -0,0 +1,3 @@
+db_user= "mern_user";
+password= "NMICTAKj3BkDklzV";
+// ?authSource=admin&replicaSet=atlas-wqqk4r-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true
